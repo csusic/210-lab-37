7b0236d9932b2c3fcd87fd18044a22eb63e308ba
Christine Susic <csusic022@insite.4cd.edu>
2026-05-14 14:40:51 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // COMSC-210 | Lab 37 | Christine Susic
 
 #include <iostream>
+#include <fstream>
 #include <string>
 using namespace std;
 
@@ -8,18 +9,34 @@ using namespace std;
 int sum_ascii(string);
 
 int main() {
-    char a = 'A';
-    cout << a << endl;
-    cout << (int) a << endl;
-    int b = 66;
-    cout << b << endl;
-    cout << (char) b << endl;
+    //read the file
+    ifstream fin("data.txt"); //open file
+    int count = 0; //counter
+    int total = 0; //initialize total to zero
+    string line; //string to read from file
+    
+    //read codes from file
+    while (getline(fin, line)) {
+        count++;
+    }
+    
+    fin.close(); //close file
+    
+    //output total
+    cout << "Total: " << total << endl;
     
     //testing function
     string x = "ABC";
     cout << "Sum x: " << sum_ascii(x) << endl;
     string y = "abc";
     cout << "Sum y: " << sum_ascii(y) << endl;
+    
+    char a = 'A';
+    cout << a << endl;
+    cout << (int) a << endl;
+    int b = 66;
+    cout << b << endl;
+    cout << (char) b << endl;
 
     return 0;
 }
