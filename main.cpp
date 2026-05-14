7713bbfb649ceb5af30a84035801e861c3b5eeab
Christine Susic <csusic022@insite.4cd.edu>
2026-05-14 15:03:09 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -14,14 +14,19 @@ int sum_ascii(string);
 int main() {
     //read the file
     ifstream fin("data.txt"); //open file
-    int count = 0; //counter
-    int total = 0; //initialize total to zero
-    string line; //string to read from file
+    //initialize count, total, and index to zero
+    int count = 0, int total = 0, int index = 0;
+    string code; //string to read from file
+    
+    //hash table map comprised of int (key) and list (values)
+    std::map<int, std::list<string>> hash_table;
     
     //read codes from file
-    while (getline(fin, line)) {
+    while (getline(fin, code)) {
         count++;
-        total += sum_ascii(line);
+        total += sum_ascii(code);
+        index += gen_hash_index(code);
+        hash_table[index].push_back(code);
     }
     
     fin.close(); //close file
@@ -29,10 +34,13 @@ int main() {
     //output total
     cout << "Total: " << total << endl;
     
-    //hash table map comprised of int (key) and list (values)
-    std::map<int, list<string>> hash_table;
-    
     //display first 100 entries
+    //access the map using iterators
+    cout << "Display first 100 entries:" << endl;
+    for (std::map<int, std::list<string>>::iterator it = hash_table.begin();
+    it != hash_table.end(); it++) {
+        cout << it->first << endl;
+    }
     
     //testing function
     string x = "ABC";
@@ -52,7 +60,7 @@ int gen_hash_index(string x) {
         //sum characters, int cast gets ASCII values
         sum += (int) c;
     }
-    return sum;
+    return sum % 1000;
 };
 
 //function to get sum of ascii values
